63736579fbb374d10e68cb099e5e4d1f26b3fd47
Emily Hyland <hello@duien.com>
2016-06-14 22:13:54 -0700

    Add more docs to test generation

--- lib/speck.ex
+++ lib/speck.ex
@@ -2,6 +2,6 @@ defmodule Speck do
   @moduledoc """
   Top-level namespace for my Slack client.
 
-  Doesn't actually do anything.
+  Doesn't actually do anything. However, it does get its docs automatically published by CI, which is spiffy.
   """
 end
